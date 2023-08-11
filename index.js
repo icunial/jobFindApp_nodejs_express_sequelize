b035c8fe0c2e8390b2b2a63d2e5c377aff94908b
Ignacio Cunial <igsecu@hotmail.com>
2023-08-11 14:34:25 -0300

    Initialized Sequelize and Tested connection -> index.js

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
 
+require("dotenv").config();
+
+// Sequelize connection
+const Sequelize = require("sequelize");
+const db = new Sequelize("postgres://postgres:telefe14@localhost:5432/jobs_db");
+
+// Test db
+db.authenticate()
+  .then(() => console.log("Database connected..."))
+  .catch((err) => console.log("Error: ", err));
+
 // Body-parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Job Find App");
+});
 
 const PORT = process.env.PORT || 5000;
 
