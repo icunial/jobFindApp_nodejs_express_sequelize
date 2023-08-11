8b9d184b8c606d6c1e2e4d9b6ff28c1c12eb4ed6
Ignacio Cunial <igsecu@hotmail.com>
2023-08-11 16:41:30 -0300

    Created GET / gigs router -> get all gigs

--- routes/gigs.js
+++ routes/gigs.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../config/database");
+const Gig = require("../models/gig");
+
+router.get("/", (req, res) => {
+  Gig.findAll()
+    .then((gigs) => {
+      console.log(gigs);
+      res.status(200);
+    })
+    .catch((error) => console.log(error));
+});
 
 module.exports = router;
